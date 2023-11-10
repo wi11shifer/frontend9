194b3c1c03862676379b41ea084315333bfd298e
willshifer <118568428+wi11shifer@users.noreply.github.com>
2023-11-10 12:34:42 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ let computerScore = 0;
 let userCards = [];
 let computerCards = [];
 let cards = [
-    {name: '6', value: 6, image: 'https://upload.wikimedia.org/wikipedia/commons/3/3c/Atlas_deck_6_of_spades.svg'},
-    {name: '7', value: 7, image: 'https://upload.wikimedia.org/wikipedia/commons/c/c1/Atlas_deck_7_of_spades.svg'},
-    {name: '8', value: 8, image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Atlas_deck_8_of_spades.svg/1200px-Atlas_deck_8_of_spades.svg.png'},
-    {name: '9', value: 9, image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Atlas_deck_9_of_spades.svg/1365px-Atlas_deck_9_of_spades.svg.png'},
-    {name: '10', value: 10, image: 'https://upload.wikimedia.org/wikipedia/commons/3/3c/Atlas_deck_6_of_spades.svg'},
-    {name: 'Валет', value: 2, image: 'https://upload.wikimedia.org/wikipedia/commons/2/2e/Atlas_deck_jack_of_spades.svg'},
-    {name: 'Дама', value: 3, image: 'https://upload.wikimedia.org/wikipedia/commons/8/8a/Atlas_deck_queen_of_spades.svg'},
-    {name: 'Король', value: 4, image: 'https://upload.wikimedia.org/wikipedia/commons/8/8b/%D0%90%D1%82%D0%BB%D0%B0%D1%81%D0%BD%D0%B0%D1%8F_%D0%BA%D0%BE%D0%BB%D0%BE%D0%B4%D0%B0_%D1%8D%D0%BA%D1%81%D1%82%D1%80%D0%B0_%D0%BA%D0%BE%D1%80%D0%BE%D0%BB%D1%8C_%D0%BF%D0%B8%D0%BA%D0%B8.jpg'},
-    {name: 'Туз', value: 11, image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Aceofspades.svg/800px-Aceofspades.svg.png'}
+    {name: '6', value: 6, image: 'images/6.svg'},
+    {name: '7', value: 7, image: 'images/7.svg'},
+    {name: '8', value: 8, image: 'images/8.png'},
+    {name: '9', value: 9, image: 'images/9.png'},
+    {name: '10', value: 10, image: 'images/10.png'},
+    {name: 'Валет', value: 2, image: 'images/valet.svg'},
+    {name: 'Дама', value: 3, image: 'images/dama.svg'},
+    {name: 'Король', value: 4, image: 'images/king.jpg'},
+    {name: 'Туз', value: 11, image: 'images/tuz.png'}
        ];
 // Отримання імені користувача
 let userName = prompt("Введіть ваше ім'я");
